01f753d40c8a07973ab06abcd35c6afc7f0c597b
Jack <jparw33@gmail.com>
2023-12-28 13:30:33 +0000

    first commit

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+export const links = [
+    {
+      label: "Email",
+      to: "mailto:j@jparw.me",
+    },
+    {
+      label: "Resume",
+      to: "#",
+    },
+    {
+      label: "Concept",
+      to: "#",
+    },
+    {
+      label: "Code",
+      to: "#",
+    },
+  ];
